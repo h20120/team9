5e7b15f77cc7da5cca1e2ca31b6c7233671eb853
tanakaryuuji <20186@hakodate.kosen-ac.jp>
2024-11-25 14:13:51 +0900

    (ペンギンのサイズを変更)

--- game9/bekkamu.java
+++ game9/bekkamu.java
@@ -6,27 +6,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class bekkamu extends Actor
 {
     /**
      * Act - do whatever the bekkamu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public bekkamu() {
+    GreenfootImage image = getImage();         // 現在の画像を取得
+    image.scale(image.getWidth() / 4, image.getHeight() / 4); // 幅と高さを半分に縮小
+    setImage(image);                          // 縮小した画像をセット
+}
+
     public void act() 
     {
-        // Add your action code here.
-        if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(0);
-        move(1);
-    }if( Greenfoot.isKeyDown( "left" ) ){
-        setRotation(-180);
-        move(1);
-    }if( Greenfoot.isKeyDown( "up" ) ){
-        setRotation(45);
-        move(1);
-    }if( Greenfoot.isKeyDown( "down" ) ){
-        setRotation(-45);
-        move(1);
-    }
+          // 左右キーで移動
+        if (Greenfoot.isKeyDown("left")) {
+            move(-5);  // 左に5ピクセル移動
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            move(5);   // 右に5ピクセル移動
+        }
+        
+        // ワールドの端を超えないようにする
+        if (getX() < 10) {
+            setLocation(10, getY());
+        }
+        if (getX() > getWorld().getWidth() - 10) {
+            setLocation(getWorld().getWidth() - 10, getY());
+        }
     }    
 }
